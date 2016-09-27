22acef292da7cb2600c27f12f8993566494d95de
Jake Young <cheetopuffgaming@gmail.com>
2016-09-27 11:01:34 -0700

    made more responses and made the response go every line

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,11 @@
 function askQuestion() {
   var input = document.getElementById("input").value;
-  document.getElementById("chat-area").textContent += input;
+  var giveresponse = responses[input]
+  document.getElementById("chat-area").innerHTML += input + ": " + giveresponse + " " + "<br>"; 
+}
+
+var responses = {
+  "how are you?" : "I am Stinky. Thank you for asking. If you would like to ask StinkyJoe more questions that is ok.",
+  "what is your age" : "I am eternal.",
+  "what is your name" : "Stinky Joe"
 }
